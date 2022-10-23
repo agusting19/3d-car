741a52e01883924e6b351bee7e8216a456583ca9
Agustin Gomez <agusleongomez@gmail.com>
2022-10-23 20:08:11 -0300

    CarShow created, textures not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
+import CarShow from "./components/CarShow";
 import "./index.css";
-
-const CarShow = () => {
-  return (
-    <>
-      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={"blue"} />
-      </mesh>
-    </>
-  );
-};
 
 function App() {
   return (
