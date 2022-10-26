2985d22bb3da6521edd990c23789496d7ef73d3f
Agustin Gomez <agusleongomez@gmail.com>
2022-10-26 20:14:15 -0300

    added cube camera

--- src/components/CarShow.jsx
+++ src/components/CarShow.jsx
@@ -1,4 +1,9 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import Car from "./Car";
 import Ground from "./Ground";
 import Rings from "./Rings";
@@ -9,7 +14,16 @@ const CarShow = () => {
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
       <color args={[0, 0, 0]} attach="background" />
-
+      <CubeCamera>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
+      <Ground />
+      <Rings />
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
@@ -28,9 +42,6 @@ const CarShow = () => {
         castShadow
         shadow-bias={-0.0001}
       />
-      <Ground />
-      <Car />
-      <Rings />
     </>
   );
 };
